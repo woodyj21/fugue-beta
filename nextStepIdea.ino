529686e8e264c5414461558ac22587773fd4f5c5
James Woody <woody.james@gmail.com>
2015-01-23 13:16:42 -0600

    Modified a "BareMinimum" sketch to begin.  This will be working section for code.  Should always be complilable.

--- nextStepIdea.ino
+++ nextStepIdea.ino
@@ -1,23 +1,41 @@
-loop{
-	readButtons()
-	readPedals()
-	sendCode()
-	updateLogic()
-	updateLCD()
+/*    This will be the working code for next phase.  Code should be compilable each time saved.
+       01/23/2015        BareMinimum, modified w/ functions to handle basic things
+*/
+
+void setup() {
 }
 
-void readButtons{				//offload logic, only read sequencially
-	for(i=0,i>4,i++){
+void loop() {
+	readButtons();
+	readPedals();
+	sendCode();
+	updateLogic();
+	updateLCD();
+}
+
+void readButtons() {				//offload logic, only read sequencially
+/*	for(i=0,i>4,i++){
 		if(button().isPressed()){
 			activeButton = i
 		}
 
-	}
+	} */
 }
 
-void readPedals{
-	if(pedalActive == true){		//make a way for by-passing
+void readPedals() {
+        boolean pedalActive;
+        
+  	if(pedalActive == true){		//make a way for by-passing
 	}
 	else{
 	}
+}
+
+void sendCode() {
+}
+
+void updateLogic() {
+}
+
+void updateLCD() {
 }
